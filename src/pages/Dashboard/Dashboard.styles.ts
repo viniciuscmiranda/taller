d0736e37103ae3db499882fcc9703e2b1f228b4b
vinciuscmiranda <viniciusc.miranda@outlook.com>
2024-10-17 18:39:51 -0300

    fix: background color

--- src/pages/Dashboard/Dashboard.styles.ts
+++ src/pages/Dashboard/Dashboard.styles.ts
@@ -3,10 +3,10 @@ import { Theme } from '../../styles/theme';
 
 export const DashboardContainer = styled.main`
   padding: 0.8rem;
+  background-color: ${Theme.colors.background};
 `;
 
 export const CardsContainer = styled.section`
-  background-color: ${Theme.colors.background};
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   gap: 0.8rem;
